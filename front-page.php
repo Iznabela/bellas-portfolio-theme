4285bfa4e45d2f9a9d9be111cbc7341e23c277ca
Isabella Pettersson <isabella.pettersso1@learnet.se>
2021-10-29 01:47:57 +0200

    gradient bar

--- front-page.php
+++ front-page.php
@@ -15,11 +15,13 @@ $projectsLink = get_field('projects-link');
     <!-- <img src="<?php //echo $backgroundImage; ?>" class="backcontainer__photo" id="backcontainer__photo"> -->
 </div>
 <section class="front">
+  <div class="front__banner">
+  </div>
   <div class="front__container">
     <h1 class="front__container__title"><?php echo $frontTitle; ?>
       <p class="front__container__title__description"><?php echo $frontDescription ?></p>
-      <a class="front__container__title__button btn btn-success" href="<?php echo $projectsLink ?>"><?php echo $projectsLink ?></a>
-    </h1>
+      <a class="front__container__title__button btn btn btn-outline-dark" href="<?php echo $projectsLink ?>"><?php echo $projectsLink ?></a>
+    </h1> 
   </div>
 </section>
 
