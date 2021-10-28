bffb3c199282b9268c1cc8c101611aece618dfc5
Isabella Pettersson <isabella.pettersso1@learnet.se>
2021-10-28 14:09:52 +0200

    background adjusted

--- front-page.php
+++ front-page.php
@@ -6,13 +6,15 @@ $backgroundImageArray = get_field('background_image');
 $backgroundImage = $backgroundImageArray['sizes']['large'];
 ?>
 
-<div class="background">
-    <img src="<?php echo $backgroundImage; ?>" class="background__img img-fluid">
+<div class="background" <?php if( $backgroundImage ) : ?>
+        style="background-image: url(<?php echo $backgroundImage; ?>);"
+    <?php endif; ?>>
+    <!-- <img src="<?php //echo $backgroundImage; ?>" class="backcontainer__photo" id="backcontainer__photo"> -->
 </div>
 <section class="front">
   <div class="front__container">
     <h1 class="front__container__title">TITLE
-      <p class="front__container__description">hallååååå</p>
+      <p class="front__container__description">tjena tjena</p>
       <a class="front__container__btn btn btn--round"></a>
       </h1>
   </div>
