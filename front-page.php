b234808288d0a49533ab30caeb08860b40fb2acb
Isabella Pettersson <isabella.pettersso1@learnet.se>
2021-10-28 21:07:42 +0200

    classname change

--- front-page.php
+++ front-page.php
@@ -12,11 +12,11 @@ $backgroundImage = $backgroundImageArray['sizes']['large'];
     <!-- <img src="<?php //echo $backgroundImage; ?>" class="backcontainer__photo" id="backcontainer__photo"> -->
 </div>
 <section class="front">
-  <div class="front__container">
-    <h1 class="front__container__title">TITLE
-      <p class="front__container__description">tjena tjena</p>
-      <a class="front__container__btn btn btn--round"></a>
-      </h1>
+  <div class="main__container">
+    <h1 class="main__container__title">TITLE
+      <p class="main__container__description">tjena tjena</p>
+      <a class="main__container__btn btn btn--round"></a>
+    </h1>
   </div>
 </section>
 
