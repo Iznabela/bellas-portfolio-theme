a7aeac2552291309ab8b02f72a8611185ea3a90e
Isabella <isabellapetterson@gmail.com>
2021-11-12 13:16:31 +0100

    single project content and link

--- includes/section-project.php
+++ includes/section-project.php
@@ -1,21 +1,27 @@
 <?php
    get_header();
 ?>
-<div>
-      <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); 
-      if ( has_post_thumbnail() ) {
-          $large_image_url = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'large' );
-          if ( ! empty( $large_image_url[0] ) ) {
-              echo '<a href="' . the_title_attribute( array( 'echo' => 0 ) ) . '" title="' . the_title_attribute( array( 'echo' => 0 ) ) . '">';
-              echo get_the_post_thumbnail( $post->ID, 'thumbnail', array( 'class' => 'projects__container__content__posts__post__img' )); 
-              echo '</a>';
-          }
-      ?> </div> <?php
-      }
-        endwhile; else: ?>
-      <p>Sorry, no posts found.<p></p>
-      <?php endif; ?>
+<section class="project">
+   <div class="project__container">
+      <div class="projects__container__content">
+      <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+         <h2 class="project__container__content__title"><?php echo the_title(); ?></h2>
+         <p class="project__container__content__description"><?php echo the_excerpt(); ?></p>
+         <?php 
+         if ( has_post_thumbnail() ) {
+               $large_image_url = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'large' );
+               if ( ! empty( $large_image_url[0] ) ) {
+                  echo '<a href="' . the_title_attribute( array( 'echo' => 0 ) ) . '" title="' . the_title_attribute( array( 'echo' => 0 ) ) . '">';
+                  echo get_the_post_thumbnail( $post->ID, 'thumbnail', array( 'class' => 'project__container__content__img' )); 
+                  echo '</a>';
+               }
+         ?> </div> <?php
+         }
+            endwhile; else: ?>
+         <p>Sorry, no posts found.<p></p>
+         <?php endif; ?>
    </div>
+</section>
 
 <?php
    get_footer();
--- includes/section-projects.php
+++ includes/section-projects.php
@@ -17,7 +17,7 @@ get_header();
         if ( has_post_thumbnail() ) {
           $large_image_url = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'large' );
           if ( ! empty( $large_image_url[0] ) ) {
-              echo '<a href="' . the_title_attribute( array( 'echo' => 0 ) ) . '" title="' . the_title_attribute( array( 'echo' => 0 ) ) . '">';
+              echo '<a href="' . get_permalink( $post->ID ) . '" title="' . the_title_attribute( array( 'echo' => 0 ) ) . '">';
               echo get_the_post_thumbnail( $post->ID, 'thumbnail', array( 'class' => 'projects__container__content__posts__post__img' )); 
               echo '</a>';
           }
